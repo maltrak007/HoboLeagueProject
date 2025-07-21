5679a506fc1702edbc11868752010df26656f3f4
4LiveStudioGit <david.alamo@gmail.com>
2025-07-21 20:37:26 +0200

    Test

--- Source/HoboLeagueProject/Character/Player/PlayerCharacter.cpp
+++ Source/HoboLeagueProject/Character/Player/PlayerCharacter.cpp
@@ -50,6 +50,7 @@ void APlayerCharacter::ServerSideInit()
 	HAbilitySystemComponent->InitAbilityActorInfo(HoboPlayerState,this);
 	HAbilitySystemComponent->ApplyInitialEffects();
 	HAbilitySystemComponent->GiveInitialAbilities();
+	//Borrar
 }
 
 void APlayerCharacter::OnRep_PlayerState()
