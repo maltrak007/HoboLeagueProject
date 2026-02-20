a74486576663669b5ce5bac66c4cd78df8c567ff
DrMalboro07 <gonza7108@gmail.com>
2026-02-20 18:44:54 +0100

    Optimized GA_Attack + Rebuild in Item Stats Access + Scenario Assets

---
+++ Source/HoboLeagueProject/Item/PlayerItem/Weapon/HWeaponStatsTable.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HWeaponStatsTable.h"
+
+
+HWeaponStatsTable::HWeaponStatsTable()
+{
+}
+
+HWeaponStatsTable::~HWeaponStatsTable()
+{
+}
